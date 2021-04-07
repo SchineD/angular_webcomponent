118415b71ebebf82d02b4f4c336d33820255c6f5
Daniel Schinewitz <daniel.schinewitz@gmx.at>
2021-04-07 16:35:49 +0200

    push to repo

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import '../web-component/auto-complete';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'AngularAutocomplete';
+}
